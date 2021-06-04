638e88297c9980a50208d57eaf8d8fee262cf02e
chenmin999 <270144864@qq.com>
2021-06-04 15:52:59 +0800

    master制造冲突

--- src/com/igeek/test/Test.java
+++ src/com/igeek/test/Test.java
@@ -11,6 +11,8 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("当前在测试Git回退版本1");
         System.out.println("当前在测试Git分支ggg操作");
+        System.out.println("++++++++++++++制造冲突+++++++++++++++");
+        System.out.println("master主分支");
     }
 
 }
